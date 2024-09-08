e53e7eaef027b28de8f3fef35e34ca9fd7d80865
@_𝚁𝚊𝚜𝚒𝚢𝚊999 <145731613+tharindumaxx123@users.noreply.github.com>
2024-09-09 00:37:23 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "62815859783969"
+const SESSION_ID = "PRABATH-MD~Zc50QDDL#Xj3YDxfHF4DK_Z0jaRPeKGGXUtWGCbKDeWPBc3UCB_s"
 
 
 module.exports = {
